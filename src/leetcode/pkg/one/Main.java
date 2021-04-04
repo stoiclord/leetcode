6b7037d142af51d3aa4b3c7e47fe7a60b32312be
David <fullstackdahveed@gmail.com>
2021-04-04 23:19:55 +0530

    project first commit

---
+++ src/leetcode/pkg/one/Main.java
@@ -0,0 +1,12 @@
+package leetcode.pkg.one;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Start solving leetcode programs");
+		
+	}
+
+}
